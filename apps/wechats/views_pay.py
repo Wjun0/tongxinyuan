4295b4b27ebf7155b377c8299d89f40c6486878e
wj <wj@163.com>
2024-10-15 19:19:51 +0800

    完成付费订单更新

--- apps/wechats/views_pay.py
+++ apps/wechats/views_pay.py
@@ -1,5 +1,4 @@
 import uuid
-
 from django.conf import settings
 from rest_framework.generics import ListAPIView, CreateAPIView
 from apps.questions.models import QuestionType_tmp, QuestionType
@@ -38,8 +37,9 @@ class PayJsApiView(CreateAPIView):
 class NotifyView(CreateAPIView):
 
     def create(self, request, *args, **kwargs):
-        LOG.info(f"支付回调通知 \r\n {request.data}")
+        LOG.info(f"支付回调通知 \r\n {request.headers} \r\n {request.data}")
         result = notify_callback(request.headers, request.data)
+        LOG.info(f"notify_callback =  \r\n {result}")
         if result and result.get('event_type') == 'TRANSACTION.SUCCESS':
             resp = result.get('resource')
             appid = resp.get('appid')
@@ -56,7 +56,13 @@ class NotifyView(CreateAPIView):
             amount = resp.get('amount').get('total')
             LOG.info(f"支付回调成功 \r\n out_trade_no={out_trade_no}")
             return Response({"message": "success"})
-        return Response({"message": "fail"}, status=500)
+        s = {'id': 'a5657519-b2c1-5f1f-b919-26cf57995de6', 'create_time': '2024-10-09T17:57:42+08:00',
+         'resource_type': 'encrypt-resource', 'event_type': 'TRANSACTION.SUCCESS', 'summary': '支付成功',
+         'resource': {'original_type': 'transaction', 'algorithm': 'AEAD_AES_256_GCM',
+                      'ciphertext': 'LblphfIG79IW638A06YFXaQVXi3gsZ+NgYOd4j5k7kRZwy8+GmQvpaTzvOR2UZhPGH81vd2kTQoXWRhydBVCybaKKeKbTktWeMmBwO7xWB5v4R139zy/0bjlzVjNxq93CHOuPtaPBVqAiFo6dUAnGknoR8anfwEXxTa4vM+1VfpO5iBgnAP8lLcc1za3gUk9OvF8lGgtaDdsbBZ/xwhl+cF5H/tjmdvLppnkp5yE8/E1nDQcFtxWlRpiq5sXhyzzPqNQDO9xNLR58wzTD+zlabNh9+EyXjL3MemQGUymKgMmR+rPQZeuxbiZW4KHWAsgHkMolRfLNtRBSG6gIciRObNOR1rMEr47WNTn3kzb8vkZIyj8jnfH1EzRfVU3+FxHzQVZUoUHNIqTkEfRc8SZLwNBjZ/O9WBT+l2HUXBvM54RXJCd9x6IPLMttJslkDiey5S0AUT3vDRlG4JH4DaxsOltbFYHIgpMHX9jEhzlZOTRijyn3tX8KKbaWFEoj4kvrs2zIKPskv3a0WXTy+xWEqHFLubXGQOKQPIyd0ITrmu56KHLHlVNZ15OD3OOxPZcfpo=',
+                      'associated_data': 'transaction', 'nonce': 'HogZtBg3vfzW'}}
+        LOG.info(f"支付回调失败 \r\n result={result}")
+        return Response({"message": "fail"}, status=200)
 
 class PayQueryView(CreateAPIView):
     permission_classes = (WexinPermission,)
@@ -77,11 +83,11 @@ class PayQueryView(CreateAPIView):
         if code in range(200,300):
             trade_state_desc = eval(message).get('trade_state_desc')
             if trade_state_desc == "订单未支付":
-                pass
-            elif trade_state_desc == "订单已支付":
+                return Response({"detail": "fail", "data": {"pay_status": "订单未支付"}})
+            elif trade_state_desc == "支付成功":
                 obj.pay_status = "已支付"
                 obj.save()
                 return Response({"detail": "success", "data":{"pay_status":"支付成功"}})
             else:
-                pass
-        return Response({"detail": "fail", "data": {"pay_status": "支付未支付"}})
+                return Response({"detail": "fail", "data": {"pay_status": "其他"}})
+        return Response({"detail": "fail", "data": {"pay_status": "订单未支付"}})
